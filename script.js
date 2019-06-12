bd119c02674a07d44b1a8c47fcc2db63df9f03f0
Miguel <m3k3r1@gmail.com>
2019-06-12 05:20:09 +0100

    12h timer

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+var schedule = require('node-schedule');
 var conf = require('./configs.js');
 var mongoose = require('mongoose');
 var User = require('./models/user')
@@ -7,36 +7,29 @@ var Device = require('./models/device')
 
 //connect to database
 mongoose.connect(conf.mongodb, {useNewUrlParser: true});
-var job = new CronJob('00 00 12 * * 0-6', function() {
-  /*
-   * Runs every day
-   * at 12:00:00 AM.
-   */
 
-  Device.find({}).then( devices => {
-    devices.forEach(device => {
-     var now = Date.now();
-     var devLastSeen = new Date(device.last_seen);
 
-     var timeDiff = Math.abs(devLastSeen.getTime() - now);
-     var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
-
-     console.log( device.device +  ">" + diffDays);
-
-     if(diffDays > 1){
-         notifyCompany(device.company, "Device " + device.name + "is down for more than  1 day");
-         Device.findByIdAndUpdate(device.id, {notifications:{deviceDown: true}});
-     }
+var downForAWeekChecker = schedule.scheduleJob('12 * * *', function(){
+    Device.find({}).then( devices => {
+        devices.forEach(device => {
+         var now = Date.now();
+         var devLastSeen = new Date(device.last_seen);
+    
+         var timeDiff = Math.abs(devLastSeen.getTime() - now);
+         var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+    
+         console.log( device.device +  ">" + diffDays);
+    
+         if(diffDays > 1){
+             notifyCompany(device.company, "Device " + device.name + "is down for more than  1 day");
+             Device.findByIdAndUpdate(device.id, {notifications:{deviceDown: true}});
+         } else {
+            Device.findByIdAndUpdate(device.id, {notifications:{deviceDown: false}});
+         }
+        })
     })
 })
 
-
-  }, function () {
-    /* This function is executed when the job stops */
-  },
-  true, /* Start the job right now */
-  timeZone /* Time zone of this job. */
-);
 
 
  
